c0ef34c929f7f00daaaec1dfcbf3548c08f2f5b7
Caio Filipe <caiofelipelg@hotmail.com>
2026-05-06 23:46:08 -0300

    chore: initial project setup with Spring Boot 3.5.14

---
+++ src/main/java/com/caiofilipe/openfinancehub/OpenfinanceHubApplication.java
@@ -0,0 +1,13 @@
+package com.caiofilipe.openfinancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfinanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfinanceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiofilipe/openfinancehub/OpenfinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiofilipe.openfinancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
